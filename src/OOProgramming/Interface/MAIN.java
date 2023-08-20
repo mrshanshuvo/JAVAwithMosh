e320fe09c9779296afb481444f58309eda45199c
Shahid Hasan Shuvo <mrshanshuvo@gmail.com>
2023-08-21 05:50:15 +0600

    Initial Commit

--- src/OOProgramming/Interface/MAIN.java
+++ src/OOProgramming/Interface/MAIN.java
@@ -4,5 +4,7 @@ public class MAIN {
         var calculator = new TaxCalculator2023(100_000);
         var report = new TaxReport(calculator);
         report.show();
+        report.setCalculator(new TaxCalculator2022(200_000));
+        report.show();
     }
 }
--- src/OOProgramming/Interface/TaxReport.java
+++ src/OOProgramming/Interface/TaxReport.java
@@ -9,4 +9,7 @@ public class TaxReport{
         var tax = calculator.calculateTax();
         System.out.println(tax);
     }
+    public void setCalculator(TaxCalculator calculator) {
+        this.calculator = calculator;
+    }
 }
